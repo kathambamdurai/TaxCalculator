5bd548d7e04ce61844379b10b7f560b92e1da5c3
duraisamytalentoz <duraisamy@talentoz.com>
2023-12-11 13:55:07 +0530

    Add project files.

---
+++ Payspace_Assessment_NUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Payspace_Assessment_NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
